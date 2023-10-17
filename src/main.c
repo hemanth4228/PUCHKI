26909dbbaec83a19e4e89d002e48d36d28415653
nishanthchowdary <60138643+nishanthchowdary@users.noreply.github.com>
2023-10-17 22:51:28 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,28 +1,27 @@
 #include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
 #include <stdio.h>
 
-// Undefine the main macro from posix_cheats.h
-#undef main
+K_TIMER_DEFINE(my_timer, NULL, NULL);
 
-int main(void) {
+void timer_handler(struct k_timer *timer_id)
+{
+    printk("Timer Expired!\n");
+}
+
+int main(void)
+{
+    printk("Zephyr Timer Example\n");
     printf("Hello World! %s\n", CONFIG_BOARD);
 
-    // Define a timer structure
-    K_TIMER_DEFINE(my_timer, NULL, NULL);
+    k_timer_init(&my_timer, timer_handler, NULL);
 
-    // Callback function to be executed when the timer expires
-    void timer_callback(struct k_timer *timer_id) {
-        printf("Timer expired! Do something here.\n");
+    k_timer_start(&my_timer, K_MSEC(1000), K_MSEC(1000));
+
+    while (1)
+    {
+        k_sleep(K_FOREVER);
     }
 
-    // Initialize the timer
-    k_timer_init(&my_timer, timer_callback, NULL);
-
-    // Start the timer with an initial timeout of 2 seconds and a period of 4 seconds
-    k_timer_start(&my_timer, K_SECONDS(2), K_SECONDS(4));
-
-    while (1) {
-        // Your main application code can run here
-        // You can perform other tasks while the timer is active
-    }
+    return 0;
 }
